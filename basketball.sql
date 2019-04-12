7d05a722d45d97e3e6269893cf568e44172c2df1
Peter <46304666+peterjorphan@users.noreply.github.com>
2019-04-11 20:11:41 -0500

    load players stat to database, reorganize folders

--- basketball.sql
+++ basketball.sql
@@ -36,6 +36,62 @@ CREATE TABLE player_data (
   namekey VARCHAR(64)
 );
 
+DROP TABLE IF EXISTS player_stats;
+CREATE TABLE player_stats (
+  id INT NOT NULL PRIMARY KEY,
+  Year INT,
+  Player TEXT, 
+  Pos TEXT, 
+  Age INT, 
+  Tm TEXT, 
+  G INT, 
+  GS INT, 
+  MP INT, 
+  PER DECIMAL(3,1), 
+  TSprc DECIMAL(4,3), 
+  3PAr DECIMAL(4,3), 
+  FTr DECIMAL(4,3),
+  ORBprc DECIMAL(3,1),
+  DRBprc DECIMAL(3,1),
+  TRBprc DECIMAL(3,1),
+  ASTprc DECIMAL(3,1),
+  STLprc DECIMAL(3,1),
+  BLKprc DECIMAL(3,1),
+  TOVprc DECIMAL(3,1),
+  USGprc DECIMAL(3,1),
+  OWS DECIMAL(3,1),
+  DWS DECIMAL(3,1),
+  WS DECIMAL(3,1),
+  WS48 DECIMAL(4,3),
+  OBPM DECIMAL(3,1),
+  DBPM DECIMAL(3,1),
+  BPM DECIMAL(3,1),
+  VORP DECIMAL(3,1),
+  FG INT,
+  FGA INT, 
+  FGprc DECIMAL(4,3),
+  3P INT, 
+  3PA INT, 
+  3Pprc DECIMAL(4,3),
+  2P DECIMAL(3,1),
+  2PA INT, 
+  2Pprc DECIMAL(3,1),
+  eFGprc DECIMAL(3,1),
+  FT INT, 
+  FTA INT,
+  FTprc DECIMAL(3,1),	
+  ORB INT, 
+  DRB INT, 
+  TRB INT, 
+  AST INT, 
+  STL INT,
+  BLK INT,
+  TOV INT,
+  PF INT,
+  PTS  INT,
+  namekey VARCHAR(64)
+);
+
 /* 
 ALTER TABLE salary 
 ADD FOREIGN KEY (namekey) REFERENCES players(namekey);
@@ -45,4 +101,5 @@ ADD FOREIGN KEY (namekey) REFERENCES players(namekey);
 
 SELECT * FROM basketball_db.players;
 SELECT * FROM basketball_db.player_data;
-SELECT * FROM basketball_db.salary;
+SELECT * FROM basketball_db.salary; 
+SELECT * FROM basketball_db.player_stats;
